45a752ec0fcca6051cc98b34886bceaacb62e5c2
chittaranjan <chittaranjan.swain@oracle.com>
2020-08-14 22:45:48 +0530

    Adding tests for routeService geocode

--- src/test/java/TestRouteService.java
+++ src/test/java/TestRouteService.java
@@ -1,2 +1,31 @@
+import model.City;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import service.RouteService;
+import service.RouteServiceImpl;
+
 public class TestRouteService {
+
+    private RouteService routeService;
+
+    @Before
+    public void setupBeforeAllTests() {
+        routeService = new RouteServiceImpl();
+    }
+
+    @Test
+    public void testGeoCodeValidCity() {
+        City city = routeService.geoCode("Berlin");
+        Assert.assertNotNull(city);
+        Assert.assertNotNull(city.getGeometry());
+        Assert.assertNotNull(city.getBoundingBox());
+        Assert.assertEquals("Berlin", city.getName());
+    }
+
+    @Test
+    public void testGeoCodeInValidCity() {
+        City city = routeService.geoCode("InvalidCity");
+        Assert.assertNull(city);
+    }
 }
